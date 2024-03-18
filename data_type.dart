2bef01cf7c32e36c1b66d09ac44b65f4681d4969
Mehedi <mehedi360272@gmail.com>
2024-03-19 00:18:18 +0600

    updated

--- data_type.dart
+++ data_type.dart
@@ -1,3 +1,29 @@
 void main(){
-    print("Hello world");
+  //int data type
+    int age = 30;
+  //double data type
+  double a  = 20.50;
+  //String data type and String always capital letter
+  String name = "Mehedi";
+  //boolean data type
+  bool value = true;
+  //List
+  List myList = ['one', 'two', 'three', 'four'];
+  //Map data type
+  Map<String, dynamic> myMap = {
+    'name': 'Mehedi',
+    'age': 25,
+  };
+  //Set data type
+  Set mySet = {'one','two','three','four'};
+
+    print(age);
+    print(a);
+    print(name);
+    print(value);
+    print(myList);
+    //list access 1st index and 2nd length
+    print(myList[1]);
+    print(myMap);
+    print(mySet);
 }
