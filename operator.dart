daad05e40f1481ef5d3617b8265687067f0ceb57
Mehedi <mehedi360272@gmail.com>
2024-03-19 12:28:21 +0600

    updated

--- operator.dart
+++ operator.dart
@@ -1,3 +1,12 @@
 void main(){
-  
+  var sum, a, b;
+  a = 10;
+  b = 5;
+  sum = a+b;
+  print("Total $sum");
+
+  //type test operator
+  String name = 'Mehedi';
+  dynamic result = name is String;
+  print(result);
 }
