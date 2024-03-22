a8b9a9e1cb38f45c00ac5f7f43fc222825316ac2
Khondokar Md. Mehedi Hasan <108589611+mehedi360272@users.noreply.github.com>
2024-03-22 18:53:42 +0600

    updated

--- operator.dart
+++ operator.dart
@@ -16,4 +16,9 @@ void main(){
   String name = 'Mehedi';
   var result = name is String;
   print(result);
+  
+  //Conditional Operator
+  var x = 10; 
+  var res = x > 12 ? "value greater than 10":"value lesser than or equal to 10"; 
+  print(res); 
 }
